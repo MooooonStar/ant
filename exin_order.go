b471cdf94c9657095b665643a11d37a7af48d80f
snow <ymx@fox.one>
2018-12-05 19:53:06 +0800

    amount

--- exin_order.go
+++ exin_order.go
@@ -49,7 +49,7 @@ func ExinTrade(amount, send, get string, trace ...string) (string, error) {
 	precision := ExinAssetPrecision(send)
 	a := number.FromString(amount).Round(precision)
 
-	log.Infof("trade in exin, %s, send %s, get %s", amount, Who(send), Who(get))
+	log.Infof("trade in exin, %s, send %s, get %s", a, Who(send), Who(get))
 	transfer := bot.TransferInput{
 		AssetId:     send,
 		RecipientId: ExinCore,
