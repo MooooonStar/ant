32381461fd6c40080853008e3cd4f2a93335a02e
snow <ymx@fox.one>
2018-12-26 22:50:35 +0800

    add reply

--- AI.go
+++ AI.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
-//TODO
 func Reply(msg string) (string, error) {
-	return "", fmt.Errorf("no reply")
+	url := fmt.Sprintf("http://api.qingyunke.com/api.php?key=free&appid=0&msg=%s", msg)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	bt, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	var Data struct {
+		Result  int    `json:"result"`
+		Content string `json:"content"`
+	}
+	err = json.Unmarshal(bt, &Data)
+	if Data.Result != 0 {
+		return "", fmt.Errorf(Data.Content)
+	}
+	return Data.Content, nil
 }
